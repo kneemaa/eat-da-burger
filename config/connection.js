6c57e647450edf0d9b7819b8d2ccf73bc740a650
Nema Darban <nema.darban@gmail.com>
2018-03-06 23:57:38 -0800

    added jawsdb config

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,18 @@
 const mysql = require("mysql");
+const JAWSDB_URL = "otmaa16c1i9nwrek.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
 
-const connection = mysql.createConnection({
-	port: 3306,
-	host: "localhost",
-	user: "basicUser",
-	password: "password",
-	database: "burger_stand"
-});
 
+if (process.env.JAWSDB_URL){
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+	const connection = mysql.createConnection({
+		port: 3306,
+		host: "localhost",
+		user: "basicUser",
+		password: "password",
+		database: "burger_stand"
+	});
+}
 connection.connect(error => {
 	if (error) throw error;
 
